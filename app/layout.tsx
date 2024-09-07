f62f023b645609438bd0ee913b1ce18cf05d8799
Adil <adil@Adils-MacBook-Air.local>
2024-09-07 13:19:19 +0530

    implement base tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "./global.css";
 import React from "react";
 
 const geistSans = localFont({
